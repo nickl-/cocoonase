fa150b91707b436b54538f969d65dd0a5d44a981
nickl- <github@jigsoft.co.za>
2014-01-11 20:05:19 +0200

    Fix masonry location

--- lib/generators/css/cocoonase/cocoonase_generator.rb
+++ lib/generators/css/cocoonase/cocoonase_generator.rb
@@ -81,7 +81,7 @@ module Css
           get 'https://raw.github.com/t0m/select2-bootstrap-css/bootstrap3/select2-bootstrap.css',
               "#{path}/stylesheets/select2-bootstrap.css"
           get 'http://masonry.desandro.com/masonry.pkgd.min.js',
-              "#{path}/stylesheets/masonry.js"
+              "#{path}/javascripts/masonry.js"
         else
           say_status :skip,'found bootstrap use --no-skip-bootstrap to install', :yellow
         end
