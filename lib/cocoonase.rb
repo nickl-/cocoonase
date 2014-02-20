42b682fd21cc1518d011f360fdccd08469e1662c
nickl- <github@jigsoft.co.za>
2014-02-20 21:42:20 +0200

    Tried to include the monkey patches but they didn't take

--- lib/cocoonase.rb
+++ lib/cocoonase.rb
@@ -9,6 +9,8 @@ module Cocoonase
     config.before_initialize do
       #require 'simple_form'
       #Dir[File.expand_path('../../app/inputs/*.rb', __FILE__)].each {|file| require file }
+      #require File.expand_path('../active_record/nested_attributes', __FILE__)
+      #require File.expand_path('../cocoonase/action_view/helpers/form_helper', __FILE__)
 
       if config.action_view.javascript_expansions
         config.action_view.javascript_expansions[:cocoonase] = %w(cocoonase)
