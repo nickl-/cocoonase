5a14e39d3185bc31b1c151015635e3ac83d5b0ee
nickl- <github@jigsoft.co.za>
2014-02-25 16:59:39 +0200

    Removal of method attribute using attributes instead

--- lib/generators/rails/cocoonase_model/templates/nested_serializer.rb
+++ lib/generators/rails/cocoonase_model/templates/nested_serializer.rb
@@ -1,10 +1,7 @@
 <% module_namespacing do -%>
 class <%= class_name %>Serializer < ActiveModel::Serializer
 <% attributes_names.map(&:inspect).each do |att| -%>
-  attribute <%= att %><%= att =~ /_/ ? "#, key: #{att.camelize(:lower)}" : '' %>
-  def id
-    object.read_attribute_for_serialization(:id)
-  end
+  attributes <%= att %><%= att =~ /_/ ? "#, key: #{att.camelize(:lower)}" : '' %>
 <% end -%>
 end
 <% end -%>
