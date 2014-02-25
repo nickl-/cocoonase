2620009ca64dce1a4fbaa7dc9a26710bc3728843
nickl- <github@jigsoft.co.za>
2014-02-25 13:08:19 +0200

    As per generator 8.1

--- lib/generators/rails/cocoonase_model/templates/nested_serializer.rb
+++ lib/generators/rails/cocoonase_model/templates/nested_serializer.rb
@@ -2,6 +2,9 @@
 class <%= class_name %>Serializer < ActiveModel::Serializer
 <% attributes_names.map(&:inspect).each do |att| -%>
   attribute <%= att %><%= att =~ /_/ ? "#, key: #{att.camelize(:lower)}" : '' %>
+  def id
+    object.read_attribute_for_serialization(:id)
+  end
 <% end -%>
 end
 <% end -%>
